610ab9050ca7cfd3c13dfd9e71c6cb298f1cf11f
Massimo Moro <mayoita@hotmail.com>
2015-02-23 15:46:16 +0100

    Initial Commit

---
+++ MyMenu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMenu
+//
+//  Created by Massimo Moro on 23/02/15.
+//  Copyright (c) 2015 MassimoMoro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
